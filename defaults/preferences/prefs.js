306fc06b6f42ea84b93f555e572fd0cde228c598
Grace Ku <cakerug@gmail.com>
2011-06-05 23:24:02 -0400

    Stripped unnecessary code generated by Add-on Builder.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -3,4 +3,4 @@ pref("extensions.simplyPinned.intpref", 0);
 pref("extensions.simplyPinned.stringpref", "A string");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.gr@ce.ku.description", "chrome://simplyPinned/locale/overlay.properties");
+pref("extensions.simplyPinned@grace.ku.description", "chrome://simplyPinned/locale/overlay.properties");

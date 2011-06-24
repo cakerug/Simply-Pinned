7dcc7b73f723b7fc2f11c7b59e3382d3ceb76992
Grace Ku <cakerug@gmail.com>
2011-06-23 23:32:55 -0400

    Reversed options. It now asks you whether you want to hide the following tabs rather than show them.
    Cleaned up the code a bit:
    - modularized setvisibility code so that it is more efficient
    - rearranged functions
    - renamed constants
    - tidied up some comments
    
    Fixed typo in README file.

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -1,92 +1,48 @@
 let simplyPinnedChrome =
 {
-    //TODO: make this not hardcoded -- or at least only hardcoded in one place
-    //this is repeated in options.xul
-    //at least, change these to CAPS_AND_UNDERSCORES to indicate constant
-    defaultElemIds : new Array("nav-bar",
-                             "PersonalToolbar",
-                             "addon-bar"),
-    neverHideElemIds : new Array("toolbar-menubar"),
+    //TODO: fix the bug where if you launch firefox to a pinned tab w/ the
+    //  bookmarks toolbar hidden, then switch tabs it screws up
+    //  - try delaying hiding elements on initial launch after something different is loaded
+    
+    //TODO: ability to toggle on and off
+    // - add toolbar button (can be added to tab bar if someone wanted to)
+    // - add hotkey for toggling
+    // - after the above is finished, add an option to enable hiding for only pinned
+    //   or for all tabs, (which enables or disables the hotkey too?)
+    
+    DEFAULT_ELEMENT_IDS     : new Array("nav-bar",
+                                        "PersonalToolbar",
+                                        "addon-bar"),
+    NEVER_HIDE_ELEMENT_IDS  : new Array("toolbar-menubar"),
 
-    otherToolbarElems : new Array(),
+    otherToolbarElems       : new Array(),
     
-    /**
-     * Sets the visibility of elements
-     * @param Boolean show True shows the toolbars, false hides.
-     *   of elements to be hidden/shown
-     */
-    setVisibility : function(show)
-    {
-        //TODO: Perhaps this is a better way to do this
-        //https://developer.mozilla.org/en/Hiding_browser_chrome
-        
-        prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                .getService(Components.interfaces.nsIPrefService)
-                .getBranch("extensions.simplypinned.");
-        
-        //TODO: there is probably a way to make this code more efficient
-        for(var i = 0; i < simplyPinnedChrome.defaultElemIds.length; i++)
-        {
-            if(prefs.getBoolPref("bool_" + simplyPinnedChrome.defaultElemIds[i]))
-            {
-                document.getElementById(simplyPinnedChrome.defaultElemIds[i])
-                    .style.display = "inherit";
-            }
-            else
-            {
-                document.getElementById(simplyPinnedChrome.defaultElemIds[i])
-                    .style.display =
-                        show ? "inherit" : "none";
-            }
-        }
-        
-        if(prefs.getBoolPref("bool_otherToolbars"))
-        {
-            for(var i = 0; i < simplyPinnedChrome.otherToolbarElems.length; i++)
-            {
-                document.getElementById(simplyPinnedChrome.otherToolbarElems[i].id)
-                    .style.display = "inherit";
-            }
-        }
-        else
-        {
-            for(var i = 0; i < simplyPinnedChrome.otherToolbarElems.length; i++)
-            {
-                document.getElementById(simplyPinnedChrome.otherToolbarElems[i].id)
-                    .style.display = show? "inherit" : "none";
-            }
-        }
-    },
-    
-    /**
-     *
-     */
     init : function()
     {
-        //POPULATING OTHER TOOLBARS
+        //POPULATING otherToolbars ARRAY
+        //- only add elements that have toolbarnames,
+        //  are not part of the never to be hidden array,
+        //  and that aren't already listed (defaultElements)
         for(var i = 0; i < document.getElementsByTagName("toolbar").length; i++)
         {
             var aToolbar = document.getElementsByTagName("toolbar")[i];
             
             var isNeverHideElem = false;
-            for(var j = 0; j < simplyPinnedChrome.neverHideElemIds.length
-                            && !isNeverHideElem ; j++)
+            for(var j = 0; j < simplyPinnedChrome.NEVER_HIDE_ELEMENT_IDS.length
+                            && !isNeverHideElem; j++)
             {
                 isNeverHideElem =
-                    (simplyPinnedChrome.neverHideElemIds[j] == aToolbar.id);
+                    (simplyPinnedChrome.NEVER_HIDE_ELEMENT_IDS[j] == aToolbar.id);
             }
             
             var isDefaultElem = false;
-            for(var j = 0; j < simplyPinnedChrome.defaultElemIds.length
-                            && !isDefaultElem; j++)
+            for(var j = 0; j < simplyPinnedChrome.DEFAULT_ELEMENT_IDS.length
+                            && !isDefaultElem && !isNeverHideElem; j++)
             {
                 isDefaultElem =
-                    (simplyPinnedChrome.defaultElemIds[j] == aToolbar.id)
+                    (simplyPinnedChrome.DEFAULT_ELEMENT_IDS[j] == aToolbar.id)
             }
             
-            //only add elements that have toolbarnames,
-            //are not part of the never to be hidden array,
-            //and that aren't already listed (defaultElements)
             if(aToolbar.getAttribute("toolbarname") != ""
                && !isNeverHideElem && !isDefaultElem)
             {
@@ -95,19 +51,14 @@ let simplyPinnedChrome =
         }
         
         //ADDING EVENT LISTENERS
+        //TODO: find out--Are event listeners automatically removed when the tab is closed?
         var container = gBrowser.tabContainer;
-        
-        //should i remove the event listeners when the tabs are closed?
-        //Are they automatically removed when the tab is closed?
-        
-        //TODO: I feel like there is a more efficient way to set up this
-        //setVisibility function too
         
         //on tab selection change
         container.addEventListener("TabSelect",
             function(event)
             {
-                simplyPinnedChrome.setVisibility(!event.target.pinned);
+                simplyPinnedChrome.setVisibilityOfAll(!event.target.pinned);
             },
             false);
         
@@ -117,7 +68,7 @@ let simplyPinnedChrome =
             {
                 if(event.target.selected)
                 {
-                    simplyPinnedChrome.setVisibility(false);
+                    simplyPinnedChrome.setVisibilityOfAll(false);
                 }
             },
             false);
@@ -127,9 +78,62 @@ let simplyPinnedChrome =
             function(event)
             {
                 if(event.target.selected)
-                    simplyPinnedChrome.setVisibility(true);
+                    simplyPinnedChrome.setVisibilityOfAll(true);
             },
             false);
+            
+    },
+    
+    /**
+     * Sets the visibility of element with id passed in
+     * @param String anElemId The id of the element you want shown/hidden
+     * @param Boolean enabled If changing the visibility is enabled for this
+     *  element, it will be hidden/shown based on the show value passed in
+     * @param Boolean show If true, the element passed in is shown.
+     *  If false, the element passed in is hidden.
+     */
+    setVisibilityOfElement : function(anElemId, enabled, show)
+    {
+        var elem = document.getElementById(anElemId);
+        if(enabled)
+        {
+            elem.style.display = show? "inherit" : "none";
+        }
+        else
+        {
+            elem.style.display = "inherit";
+        }
+    },
+    
+    /**
+     * Sets the visibility of elements belonging to the DEFAULT_ELEMENT_IDS
+     *  and otherToolbarElems arrays
+     * @param Boolean show True shows the toolbars, false hides.
+     *  of elements to be hidden/shown
+     */
+    setVisibilityOfAll : function(show)
+    {
+        prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                .getService(Components.interfaces.nsIPrefService)
+                .getBranch("extensions.simplypinned.");
+                
+        //SETTING VISIBILITY OF DEFAULT ELEMENTS
+        for(var i = 0; i < simplyPinnedChrome.DEFAULT_ELEMENT_IDS.length; i++)
+        {
+            simplyPinnedChrome.setVisibilityOfElement(
+                simplyPinnedChrome.DEFAULT_ELEMENT_IDS[i],
+                prefs.getBoolPref("bool_" + simplyPinnedChrome.DEFAULT_ELEMENT_IDS[i]),
+                show)
+        }
+        
+        //SETTING VISIBILITY OF OTHER TOOLBARS
+        for(var i = 0; i < simplyPinnedChrome.otherToolbarElems.length; i++)
+        {
+            simplyPinnedChrome.setVisibilityOfElement(
+                simplyPinnedChrome.DEFAULT_ELEMENT_IDS[i],
+                prefs.getBoolPref("bool_otherToolbars"),
+                show)
+        }
     }
 }
 
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,4 @@
-pref("extensions.simplypinned.bool_nav-bar",            false   );
-pref("extensions.simplypinned.bool_PersonalToolbar",    false   );
-pref("extensions.simplypinned.bool_addon-bar",          true    );
-pref("extensions.simplypinned.bool_otherToolbars",      false   );
+pref("extensions.simplypinned.bool_nav-bar",         true  );
+pref("extensions.simplypinned.bool_PersonalToolbar", true  );
+pref("extensions.simplypinned.bool_addon-bar",       false );
+pref("extensions.simplypinned.bool_otherToolbars",   true  );

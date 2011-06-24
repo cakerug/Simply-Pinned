7494b341e06a79fc26d02abaa11f20663bdcf6ae
Grace Ku <cakerug@gmail.com>
2011-06-23 20:49:48 -0400

    Hide other toolbars checkbox when there are no other toolbars.

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -1,11 +1,8 @@
 let simplyPinnedOptions =
 {
-    /**
-     *
-     */
     init : function()
     {
-        //GRABBING OTHER TOOLBAR ARRAY FROM BROWSER WINDOW
+        //GRABBING OTHER TOOLBARS ARRAY FROM BROWSER WINDOW
         var browserWindow = Components
                             .classes["@mozilla.org/appshell/window-mediator;1"]
                             .getService(Components.interfaces.nsIWindowMediator)
@@ -15,24 +12,17 @@ let simplyPinnedOptions =
         //POPULATING LIST OF OTHER TOOLBARS TO BE DISPLAYED IN OPTIONS PANE
         if(otherToolbars.length == 0)
         {
-            //TODO: Ideally, this should hide instead of disable.. Plus the disabling
-            // is not very noticeable (not greyed out or anything... maybe should add greying out here)
-            document.getElementById("chk_otherToolbars").disabled = true;
-            var aLabel = document.createElement("label");
-            aLabel.setAttribute("value", "none");
-            aLabel.style.paddingLeft = "25px"; //TODO: make this not hardcoded... if possible somehow grab from the checkbox width...
-            pane.appendChild(aLabel);
+            document.getElementById("chk_otherToolbars").style.display = "none";
         }
         else
         {
-            var pane = document.getElementById("optionsPane");
             for(var i = 0; i < otherToolbars.length; i++)
             {
                 var aLabel = document.createElement("label");
                 aLabel.setAttribute("value",
                                     otherToolbars[i].getAttribute("toolbarname"));
                 aLabel.style.paddingLeft = "25px"; //TODO: make this not hardcoded... if possible somehow grab from the checkbox width...
-                pane.appendChild(aLabel);
+                document.getElementById("optionsPane").appendChild(aLabel);
             }
         }
     }
